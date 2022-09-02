26ae65a9732b4919cad9fdee097c7bb5346acc64
Chitrakshi <chitrakshi.chhabra@gmail.com>
2022-09-02 09:42:12 +0530

    First Commit - Created the GUI

---
+++ app/src/main/java/com/example/companyapplication/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.companyapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button sign_in;
+    private TextView sign_up;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sign_in = findViewById(R.id.sign_in);
+        sign_up = findViewById(R.id.sign_up);
+
+        sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                BlankFragment blankFragment = new BlankFragment();
+                fragmentTransaction.add(R.id.frameLayout, blankFragment, null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
